1d12b007a8d1d61512fc6b78550af7f8ac3477bf
kenji <gunsecondstage0106@gmail.com>
2019-07-23 22:32:33 +0900

    このアプリのスタイルだとFancyナビゲーションはキャラが隠れてしまってダメなので標準のBottomNavigationBarで実装することにした。

--- lib/romasaga/ui/top_page.dart
+++ lib/romasaga/ui/top_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 
 import 'char_list_tab.dart';
 import 'search/search_list_tab.dart';
@@ -20,31 +19,27 @@ class _TopPageState extends State<TopPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(child: _showBottomMenu(_currentIndex)),
+        child: Center(child: _menuView(_currentIndex)),
       ),
-      bottomNavigationBar: _fancyBottomNavigation(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: _allDestinations.map((destination) {
+          return BottomNavigationBarItem(
+            title: Text(destination.title),
+            icon: Icon(destination.icon),
+            backgroundColor: destination.color,
+          );
+        }).toList(),
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 
-  Widget _fancyBottomNavigation() {
-    return FancyBottomNavigation(
-      tabs: [
-        TabData(iconData: Icons.person, title: 'Char'),
-        TabData(iconData: Icons.search, title: 'Search'),
-        TabData(iconData: Icons.settings, title: 'Setting'),
-        TabData(iconData: Icons.mail, title: 'Letter')
-      ],
-      initialSelection: 0,
-      circleColor: Colors.white30,
-      onTabChangedListener: (position) {
-        setState(() {
-          _currentIndex = position;
-        });
-      },
-    );
-  }
-
-  Widget _showBottomMenu(int index) {
+  Widget _menuView(int index) {
     switch (index) {
       case 0:
         return CharListTab();
@@ -52,14 +47,28 @@ class _TopPageState extends State<TopPage> {
         return Consumer<CharListViewModel>(builder: (_, viewModel, child) {
           return SearchListTab(viewModel.findAll());
         });
-      // TODO case 2: 設定画面作成予定
-      case 3:
-        return LetterTab();
-      default:
+      case 2:
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[Text('未実装')],
         );
+      case 3:
+        return LetterTab();
     }
   }
 }
+
+class Destination {
+  final String title;
+  final IconData icon;
+  final Color color;
+  const Destination(this.title, this.icon, this.color);
+}
+
+const _allDestinations = <Destination>[
+  // TODO 色もうちょっと考えたい
+  Destination('Char', Icons.person, Colors.black12),
+  Destination('Search', Icons.search, Colors.black12),
+  Destination('Setting', Icons.settings, Colors.black12),
+  Destination('Letter', Icons.mail, Colors.black12),
+];
