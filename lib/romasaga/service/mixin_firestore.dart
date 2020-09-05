5ce5e2b623e0b4cf623a3f5477bcf914b97036b7
kenji <gunsecondstage0106@gmail.com>
2020-09-05 12:53:29 +0900

    fix: crashlyticsを入れるにあたってfirebase_coreのバージョンを上げる必要があったのでそれに伴う他のFirebaseライブラリを更新した

--- lib/romasaga/service/mixin_firestore.dart
+++ lib/romasaga/service/mixin_firestore.dart
@@ -2,14 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rsapp/romasaga/model/status.dart';
 
 class RSFirestoreMixin {
-  Future<void> setMyStatuses(List<MyStatus> myStatuses, String uid) async {
-    for (var myStatus in myStatuses) {
-      await Firestore().collection(_rootCollectionName).document(uid).collection(_statusCollectionName).document(myStatus.id.toString()).setData(_toMap(myStatus));
-    }
-  }
-
   static final String _rootCollectionName = 'backup';
   static final String _statusCollectionName = 'statuses';
+
+  Future<void> setMyStatuses(List<MyStatus> myStatuses, String uid) async {
+    for (var myStatus in myStatuses) {
+      await FirebaseFirestore.instance.collection(_rootCollectionName).doc(uid).collection(_statusCollectionName).doc(myStatus.id.toString()).set(_toMap(myStatus));
+    }
+  }
 
   Map<String, dynamic> _toMap(MyStatus myStatus) {
     return <String, dynamic>{
@@ -28,9 +28,9 @@ class RSFirestoreMixin {
   }
 
   Future<List<MyStatus>> getMyStatues(String uid) async {
-    final snapshots = await Firestore().collection(_rootCollectionName).document(uid).collection(_statusCollectionName).getDocuments();
+    final snapshots = await FirebaseFirestore.instance.collection(_rootCollectionName).doc(uid).collection(_statusCollectionName).get();
     final results = <MyStatus>[];
-    for (var doc in snapshots.documents) {
+    for (var doc in snapshots.docs) {
       results.add(_toMyStatus(doc));
     }
     return results;
@@ -38,18 +38,18 @@ class RSFirestoreMixin {
 
   MyStatus _toMyStatus(DocumentSnapshot doc) {
     return MyStatus(
-      int.parse(doc.documentID),
-      doc['hp'] as int,
-      doc['str'] as int,
-      doc['vit'] as int,
-      doc['dex'] as int,
-      doc['agi'] as int,
-      doc['intelligence'] as int,
-      doc['spirit'] as int,
-      doc['love'] as int,
-      doc['attr'] as int,
-      doc['haveChar'] as bool,
-      doc['favorite'] as bool,
+      int.parse(doc.id),
+      doc.get('hp') as int,
+      doc.get('str') as int,
+      doc.get('vit') as int,
+      doc.get('dex') as int,
+      doc.get('agi') as int,
+      doc.get('intelligence') as int,
+      doc.get('spirit') as int,
+      doc.get('love') as int,
+      doc.get('attr') as int,
+      doc.get('haveChar') as bool,
+      doc.get('favorite') as bool,
     );
   }
 }
