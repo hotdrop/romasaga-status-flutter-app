cc7de8630a1d8d1e576d1ad4a9ebd290185a326f
kenji <hotdrop.tkenji@gmail.com>
2022-05-03 11:33:53 +0900

    fix: キャラ一覧の並び順をstaticで定義していて微妙だったので拡張関数に切り出した

--- lib/ui/character/characters_view_model.dart
+++ lib/ui/character/characters_view_model.dart
@@ -29,62 +29,39 @@ class _CharactersViewModel extends StateNotifier<AsyncValue<void>> {
   Future<void> selectOrder(CharacterListOrderType type) async {
     await _read(appSettingsProvider.notifier).setCharacterListOrder(type);
   }
-
-  static List<Character> order(List<Character> c, CharacterListOrderType orderType) {
-    switch (orderType) {
-      case CharacterListOrderType.hp:
-        c.sort((c1, c2) {
-          final t = c2.myStatus?.hp ?? 0;
-          final v = c1.myStatus?.hp ?? 0;
-          return t.compareTo(v);
-        });
-        break;
-      case CharacterListOrderType.production:
-        c.sort((c1, c2) => c1.id.compareTo(c2.id));
-        break;
-      case CharacterListOrderType.status:
-        c.sort((c1, c2) {
-          final t = c2.myStatus?.sumWithoutHp() ?? 0;
-          final v = c1.myStatus?.sumWithoutHp() ?? 0;
-          return t.compareTo(v);
-        });
-        break;
-    }
-    return c;
-  }
 }
 
 // ステータス上昇（育成期間）キャラ一覧
-final charactersStatusUpStateProvider = StateProvider((ref) {
+final charactersStatusUpStateProvider = Provider((ref) {
   final c = ref.watch(characterNotifierProvider).where((c) => c.statusUpEvent).toList();
   final orderType = ref.watch(appSettingsProvider).characterListOrderType;
-  return _CharactersViewModel.order(c, orderType);
+  return c.order(orderType);
 });
 
 // 高難易度キャラ一覧
-final charactersHighLevelStateProvider = StateProvider((ref) {
+final charactersHighLevelStateProvider = Provider((ref) {
   final c = ref.watch(characterNotifierProvider).where((c) => (c.myStatus?.favorite ?? false) && (c.myStatus?.useHighLevel ?? false)).toList();
   final orderType = ref.watch(appSettingsProvider).characterListOrderType;
-  return _CharactersViewModel.order(c, orderType);
+  return c.order(orderType);
 });
 
 // 周回キャラ一覧
-final charactersForRoundStateProvider = StateProvider((ref) {
+final charactersForRoundStateProvider = Provider((ref) {
   final c = ref.watch(characterNotifierProvider).where((c) => (c.myStatus?.favorite ?? false) && !(c.myStatus?.useHighLevel ?? false)).toList();
   final orderType = ref.watch(appSettingsProvider).characterListOrderType;
-  return _CharactersViewModel.order(c, orderType);
+  return c.order(orderType);
 });
 
 // お気に入りキャラ一覧
-final charactersFavoriteStateProvider = StateProvider((ref) {
+final charactersFavoriteStateProvider = Provider((ref) {
   final c = ref.watch(characterNotifierProvider).where((c) => c.myStatus?.favorite ?? false).toList();
   final orderType = ref.watch(appSettingsProvider).characterListOrderType;
-  return _CharactersViewModel.order(c, orderType);
+  return c.order(orderType);
 });
 
 // お気に入りでないキャラ一覧
-final charactersNotFavoriteStateProvider = StateProvider((ref) {
+final charactersNotFavoriteStateProvider = Provider((ref) {
   final c = ref.watch(characterNotifierProvider).where((c) => !(c.myStatus?.favorite ?? false)).toList();
   final orderType = ref.watch(appSettingsProvider).characterListOrderType;
-  return _CharactersViewModel.order(c, orderType);
+  return c.order(orderType);
 });
