0ba9ae04de0ae50a97243d93dd476cc191ecddc6
kenji <hotdrop.tkenji@gmail.com>
2023-01-15 12:34:25 +0900

    feat: 簡易ノートをリファクタリングした。 また、これまで編集内容を自動で保存するようにしていたが間違えた時にどうにもならないのが辛すぎたので保存ボタンと戻すボタンを追加した。

--- lib/ui/note/note_page.dart
+++ lib/ui/note/note_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rsapp/res/rs_strings.dart';
 import 'package:rsapp/ui/note/note_providers.dart';
+import 'package:rsapp/ui/widget/app_button.dart';
 import 'package:rsapp/ui/widget/text_form_field.dart';
 import 'package:rsapp/ui/widget/view_loading.dart';
 
@@ -33,12 +34,45 @@ class _ViewBody extends ConsumerWidget {
       child: Column(
         children: [
           Expanded(
-            child: RSMultiLineTextField(
-              initValue: ref.watch(noteProvider),
-              onChanged: (String v) {
-                ref.read(noteControllerProvider.notifier).input(v);
-              },
+            child: TextField(
+              controller: ref.watch(noteTextEditControllerProvider),
+              maxLines: null,
+              expands: true,
+              textAlignVertical: TextAlignVertical.top,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
             ),
+          ),
+          const _ViewButtonArea(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ViewButtonArea extends ConsumerWidget {
+  const _ViewButtonArea();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isSave = ref.watch(noteIsUpdateCurrentNoteProvider);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          AppIconButton(
+            label: '保存する',
+            icon: Icons.save,
+            onTap: isSave ? ref.read(noteControllerProvider.notifier).save : null,
+          ),
+          const SizedBox(width: 8),
+          AppIconButton(
+            label: '戻す',
+            icon: Icons.restore,
+            onTap: ref.read(noteControllerProvider.notifier).clear,
           ),
         ],
       ),
